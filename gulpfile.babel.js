ceb9af9f1485ac2ee5885477fbc95f78db012863
Sergio <sergio@seedtag.com>
2016-02-28 22:09:52 +0100

    Code added

---
+++ gulpfile.babel.js
@@ -0,0 +1,52 @@
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+var argv = require('yargs').argv;
+
+gulp.task('clean', del.bind(null, ['dist']));
+
+gulp.task('serve', () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: 'modules'
+    }
+  });
+
+  gulp.watch([
+    'modules/**/*',
+  ]).on('change', reload);
+
+});
+
+gulp.task('serve:dist', ['build'], () => {
+  browserSync({
+    notify: false,
+    port: 9000,
+    server: {
+      baseDir: 'dist'
+    }
+  });
+});
+
+gulp.task('build', () => {
+  console.log(argv);
+  return gulp.src('modules/module' + argv.module + '/*')
+    .pipe(gulp.dest('dist/module' + argv.module));
+});
+
+gulp.task('deploy', ['build'], () => {
+  return gulp.src('dist')
+    .pipe($.subtree())
+    .pipe($.clean());
+});
+
+gulp.task('default', ['clean'], () => {
+  gulp.start('build');
+});
