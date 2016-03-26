3dd02aaa06c11626215e273fe14b83a7422f06ec
Sergio <sergio@seedtag.com>
2016-03-27 00:04:14 +0100

    gulp zip added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -37,8 +37,10 @@ gulp.task('serve:dist', ['build'], () => {
 
 gulp.task('build', () => {
   console.log(argv);
-  return gulp.src('modules/module' + argv.module + '/*')
-    .pipe(gulp.dest('dist/module' + argv.module));
+  return gulp.src(`modules/module${argv.module}/*`)
+    .pipe(gulp.dest(`dist/module${argv.module}`))
+    .pipe($.zip(`module${argv.module}.zip`))
+    .pipe(gulp.dest(`dist/module${argv.module}`));
 });
 
 gulp.task('deploy', ['build'], () => {
